8fb2fb1a9a416b9b5d53b3cf9152886570501aeb
anoemib <alejandro.noemi@gmail.com>
2020-09-15 19:36:20 -0300

    Se incorpora el autoscroll

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', {delay: 500});
 ScrollReveal().reveal('.cards-banner-one', {delay: 500});
 ScrollReveal().reveal('.cards-banner-two', {delay: 500});
+
+
+document.querySelector('.prueba1').addEventListener('click', () =>{
+//console.log("prueba");//
+window.scroll({
+  top: 1000,
+  left: 100,
+  behavior: 'smooth'
+});
+
+
+});
